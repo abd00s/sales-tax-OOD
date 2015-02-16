81373317955d4a8896eb48aeeb0c6e461bdc036b
Abdullah <abd00s@users.noreply.github.com>
2015-02-16 17:13:53 -0500

    Add praser class and spec; define regexes to extract item details

---
+++ parser.rb
@@ -0,0 +1,18 @@
+class Parser
+  def initialize(file)
+    @file = file
+  end
+
+  def extract_item_details
+    # @quantity = ][.match(/\d+/)
+  #   @import = if ][.match(/import/) 
+  #     true 
+  #   else 
+  #     false
+  #   end
+  # @price = ][.match(/(\d*)(.)(\d+)$/).to_s.to_f
+  # @name = ][.match(/\d+(.*)at/m)[1].strip
+  end
+end
+
+

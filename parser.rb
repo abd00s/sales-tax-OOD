531a6c9a2a1d559ebcc8083ced1d31bf4f51a089
Abdullah <abd00s@users.noreply.github.com>
2015-02-16 17:49:49 -0500

    Add parser methods

--- parser.rb
+++ parser.rb
@@ -1,18 +1,39 @@
 class Parser
+  require './item'
   def initialize(file)
+    @products = []
     @file = file
+    split_input
   end
 
-  def extract_item_details
-    # @quantity = ][.match(/\d+/)
-  #   @import = if ][.match(/import/) 
-  #     true 
-  #   else 
-  #     false
-  #   end
-  # @price = ][.match(/(\d*)(.)(\d+)$/).to_s.to_f
-  # @name = ][.match(/\d+(.*)at/m)[1].strip
+  def split_input
+    File.open("input/#{@file}") do |file|
+      @items = file.map { |line| line.strip }
+      inspect_line_items
+    end
+  end
+
+  def inspect_line_items
+    @items.each do |line_item|
+      extract_item_details(line_item)
+    end
+  end
+
+  def extract_item_details(item)
+    quantity = item.match(/\d+/)
+    import = if item.match(/import/) 
+      true 
+    else 
+      false
+    end
+    price = item.match(/(\d*)(.)(\d+)$/).to_s.to_f
+    name = item.match(/\d+(.*)at/m)[1].strip
+    create_item(name, price, quantity, import)
+  end
+
+  def create_item(name, price, quantity, import)
+    @product = Item.new(name, price, quantity, import)
+    @products << @product
   end
 end
-
 

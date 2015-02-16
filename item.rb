33f93acd01fd131e00c1806f8ee7892317505959
Abdullah <abd00s@users.noreply.github.com>
2015-02-15 19:16:50 -0500

    Defining Item methods

--- item.rb
+++ item.rb
@@ -1,8 +1,48 @@
 class Item
-  def initialize(name, price, quantity, imported?)
+  def initialize(name, price, quantity, imported)
     @name = name
     @price = price
     @quantity = quantity
-    @imported = imported?
+    @imported = imported
+  end
+
+  def sub_total
+    @price * @quantity
+  end
+
+  def import_duty
+    if @imported 
+      0.05
+    else
+      0
+    end
+  end
+
+  def is_tax_exempt?
+    @name =~ /book|pill|chocolate/
+  end
+
+  def sales_tax
+    unless is_tax_exempt? 
+      0.1 
+    else
+      0
+    end
+  end
+
+  def grand_total
+    sub_total + total_tax_amount
+  end
+
+  def total_tax_amount
+    apply_sales_tax + apply_import_duty
   end
 end
+
+product = Item.new("potato", 25, 3, true)
+puts product.sub_total
+puts product.import_duty
+puts product.is_tax_exempt? 
+puts product.sales_tax
+# puts product.grand_total
+# puts product.total_tax_amount

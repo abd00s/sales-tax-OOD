bfd44c4ce2fdba8808afe3087fe221d49c21770f
Abdullah <abd00s@users.noreply.github.com>
2015-02-16 16:24:14 -0500

    Add receipt class and spec

---
+++ receipt.rb
@@ -0,0 +1,19 @@
+class Receipt
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def add_item
+    
+  end
+
+  def display_receipt
+    
+  end
+
+  def self.run
+    Receipt.new
+  end
+end

eb64c0d6f912f49eaa74078f1094117dff1b94c6
Abdullah <abd00s@users.noreply.github.com>
2015-02-17 12:48:03 -0500

    Ran 3 text inputs through program succesfully

--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -31,5 +31,22 @@ RSpec.describe Receipt do
     end
   end
 
+  describe 'display_receipt' do
+    it 'TEST1 Outputs the line items, total tax and total amount in the desired format' do
+      shopping_cart.add_items(test1)
+      expect(shopping_cart.display_receipt).to eq("1 book: 12.49\n1 music CD: 16.49\n1 chocolate bar: 0.85\nSales Taxes: 1.50\nTotal: 29.83")
+    end
+
+    it 'TEST2 Outputs the line items, total tax and total amount in the desired format' do
+      shopping_cart.add_items(test2)
+      expect(shopping_cart.display_receipt).to eq("1 imported box of chocolates: 10.50\n1 imported bottle of perfume: 54.65\nSales Taxes: 7.65\nTotal: 65.15")
+    end
+
+    it 'TEST3 Outputs the line items, total tax and total amount in the desired format' do
+      shopping_cart.add_items(test3)
+      expect(shopping_cart.display_receipt).to eq("1 imported bottle of perfume: 32.19\n1 bottle of perfume: 20.89\n1 packet of headache pills: 9.75\n1 imported box of chocolates: 11.85\nSales Taxes: 6.70\nTotal: 74.68")
+    end
+  end
+
   
 end

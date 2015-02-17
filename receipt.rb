46ca14516eca72842df522431c907261c60ff09e
Abdullah <abd00s@users.noreply.github.com>
2015-02-16 19:27:59 -0500

    Add display receipt, taxes, total methods to Receipt

--- receipt.rb
+++ receipt.rb
@@ -1,17 +1,38 @@
 class Receipt
   attr_reader :items
   require "./parser"
+  require "./item"
 
   def initialize
     @items = []
+    @sales_taxes = 0
+    @total = 0
   end
 
   def add_items(file)
-    @items << parser_invoker(file)
+    parser_invoker(file).each {|item| @items << item}
   end
 
   def display_receipt
-    
+    @items.each do |item|
+      puts "#{item.quantity} #{item.name}: #{item.price}"
+    end
+    puts "Sales Taxes: " + calculate_sales_taxes
+    puts "Total: " + calculate_total
+  end
+
+  def calculate_sales_taxes
+    @items.each do |item|
+      @sales_taxes += item.total_tax_amount
+    end
+    @sales_taxes.to_s    
+  end
+
+  def calculate_total
+    @items.each do |item|
+      @total += item.grand_total
+    end
+    @total.to_s    
   end
 
   def self.run
@@ -23,3 +44,6 @@ class Receipt
   end
 end
 
+rec = Receipt.run
+rec.add_items("input3.txt")
+rec.display_receipt
